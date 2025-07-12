33f89711ae5dd4e8688effe4a6bdc0d87a36847f
hkadakia05 <144747086+hkadakia05@users.noreply.github.com>
2025-07-11 19:34:50 -0700

    links and tag

---
+++ src/components/SocialLinks.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default function SocialLinks() {
+  return (
+    <div className="social-links">
+      <a href="https://linkedin.com/in/helikadakia/" target="_blank" rel="noreferrer">
+        <i className="fab fa-linkedin"></i>
+      </a>
+      <a href="https://github.com/hkadakia05" target="_blank" rel="noreferrer">
+        <i className="fab fa-github"></i>
+      </a>
+      <a href="helikadakia05@gmail.com">
+        <i className="fas fa-envelope"></i>
+      </a>
+    </div>
+  );
+}
