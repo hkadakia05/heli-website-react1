eef91543be87d1b09ef74cfb928aa15d9e07337a
hkadakia05 <144747086+hkadakia05@users.noreply.github.com>
2025-07-11 20:34:47 -0700

    redid entire nav bar +body css :(

--- src/components/ThemeToggle.js
+++ src/components/ThemeToggle.js
@@ -1,12 +1,15 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 export default function ThemeToggle() {
   const [dark, setDark] = useState(true);
+
+  useEffect(() => {
+    document.body.className = dark ? 'dark' : 'light';
+  }, [dark]);
+
   return (
-    
     <button className='theme-toggle' onClick={() => setDark(!dark)}>
       {dark ? '🌙' : '☀️'}
     </button>
-
   );
 }
