b4d434e327b0bd270676d0c888f55e4055fbc2cc
hkadakia05 <144747086+hkadakia05@users.noreply.github.com>
2025-07-12 12:30:19 -0700

    about me section

---
+++ src/components/About.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+export default function About() {
+  return (
+    
+      <div className='aboutme'>
+        <h1> ABOUT ME </h1>
+        <p> Hello my name is Heli and this website is under development</p>
+      </div>
+
+    
+  );
+}
