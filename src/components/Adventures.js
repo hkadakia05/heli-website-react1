449bf3c72bd40c70a01eb95160205b348ae93574
hkadakia05 <144747086+hkadakia05@users.noreply.github.com>
2025-12-19 21:11:35 -0800

    update image carousel

--- src/components/Adventures.js
+++ src/components/Adventures.js
@@ -8,14 +8,15 @@ export default function Adventures() {
       <div className="carousel">
         <div className="carousel-track">
           {/* first set */}
-          <img src="/img/coast.jpeg" alt="Coast" />
-          <img src="/img/run.jpg" alt="Run" />
-          <img src="/img/l1rocket.jpg" alt="Rocket" />
+          <img src={`${process.env.PUBLIC_URL}/img/coast.jpeg`} alt="Coast" />
+          <img src={`${process.env.PUBLIC_URL}/img/run.jpg`} alt="Run" />
+          <img src={`${process.env.PUBLIC_URL}/img/l1rocket.jpg`} alt="Rocket" />
+
 
           {/* duplicate set for seamless loop */}
-          <img src="/img/coast.jpeg" alt="Coast" />
-          <img src="/img/run.jpg" alt="Run" />
-          <img src="/img/l1rocket.jpg" alt="Rocket" />
+          <img src={`${process.env.PUBLIC_URL}/img/coast.jpeg`} alt="Coast" />
+          <img src={`${process.env.PUBLIC_URL}/img/run.jpg`} alt="Run" />
+          <img src={`${process.env.PUBLIC_URL}/img/l1rocket.jpg`} alt="Rocket" />
         </div>
       </div>
 
